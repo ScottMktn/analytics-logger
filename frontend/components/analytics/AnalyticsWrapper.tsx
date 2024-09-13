58187ade0eb5015504243d63e257d6f5f9e26bcc
khoa <scottmktn@gmail.com>
2024-09-13 10:39:27 -0700

    temporarily remove analytics

--- frontend/components/analytics/AnalyticsWrapper.tsx
+++ frontend/components/analytics/AnalyticsWrapper.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Analytics } from "../../../package/index";
+// import { Analytics } from "../../../package/index";
 
 export default function AnalyticsWrapper() {
-  return <Analytics apiKey={"mock_b2a74d98a1ff6268e01ae447c4064a60"} />;
+  return <></>;
+  // return <Analytics apiKey={"mock_b2a74d98a1ff6268e01ae447c4064a60"} />;
 }
