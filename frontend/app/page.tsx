5fcf18ae56653b8c7c9065822190b30e2367bec3
khoa <scottmktn@gmail.com>
2024-09-13 10:52:03 -0700

    revert

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,39 @@ import { Button } from "@/components/ui/button";
 
 export default async function Index() {
   return (
-    <div className="flex-1 w-full flex flex-col gap-16 items-center">hello</div>
+    <div className="flex-1 w-full flex flex-col gap-16 items-center">
+      <nav className="w-full flex justify-center border-b border-gray-300 h-16">
+        <div className="w-full max-w-4xl flex justify-between items-center text-sm p-4">
+          <p>Logo</p>
+          <AuthButton />
+        </div>
+      </nav>
+
+      <div className="flex-1 flex flex-col gap-16 max-w-4xl p-4 w-full">
+        <main className="flex-1 flex flex-col gap-4 h-full w-full">
+          <h1 className="font-bold text-4xl tracking-tight antialiased">
+            Understand your web traffic effortlessly
+          </h1>
+          <h2 className="text-xl mb-4 text-gray-500">
+            The free and open source alternative to Vercel Analytics
+          </h2>
+          <Button>Click Me</Button>
+        </main>
+      </div>
+
+      <footer className="w-full border-t border-gray-300 p-8 flex justify-center text-center text-xs">
+        <p>
+          Powered by{" "}
+          <a
+            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+            target="_blank"
+            className="font-bold hover:underline"
+            rel="noreferrer"
+          >
+            Supabase
+          </a>
+        </p>
+      </footer>
+    </div>
   );
 }
